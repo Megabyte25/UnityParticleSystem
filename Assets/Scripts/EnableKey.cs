8889516c6b2941add087307d6e229d2e41234df2
Gary Chang <gary.chang.dev@gmail.com>
2020-08-15 02:24:12 -0400

    Implemented particle systems with multiple scenes to demo them

---
+++ Assets/Scripts/EnableKey.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnableKey : MonoBehaviour
+{
+    public GameObject objectToEnable;
+
+    void Update()
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            objectToEnable.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Explosion : MonoBehaviour
+{
+    public Transform pointOfExplosion;
+    public float explosionForce;
+    public float explosionRadius;
+
+    [SerializeField] private GameObject explosionPrefab;
+
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Instantiate(explosionPrefab, pointOfExplosion);
+
+            Collider[] colliders = Physics.OverlapSphere(pointOfExplosion.position, explosionRadius);
+            foreach (Collider c in colliders)
+            {
+                if (c.attachedRigidbody != null)
+                {
+                    c.attachedRigidbody.AddExplosionForce(explosionForce, pointOfExplosion.position, explosionRadius);
+                }
+            }
+        }
+    }
+}
